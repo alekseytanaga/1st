c1815490672c40ec1f34b4d0e7ea21ca942df456
alexeytanaga <75609045+alekseytanaga@users.noreply.github.com>
2020-12-16 14:58:55 +0300

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,24 @@
+let i = 0;
+while (i < 3) {
+    alert(i);
+    i++;
+}
+
+let a = 10;
+while (a > 5) {
+    alert (a);
+    a--;
+}
+
+let b = 0;
+do {
+    alert (b);
+    b++;
+} while (b < 3);
+
+let c = 0;
+do {
+    alert(c);
+    c++;
+} while (c < 3);
+
